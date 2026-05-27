b1c87cfb316552f8aa2fc1e52be19c4eddbe3bb0
Jan Kupke <jan.kupke@web.de>
2026-05-27 16:00:41 +0200

    Add more permutations
    Add more permutations and fix broken ones that forgot some stickers.

--- src/permutations/permutations.c
+++ src/permutations/permutations.c
@@ -2,7 +2,7 @@
 #include "../cube/cube.h"
 
 void apply_move(sticker_color_t cube[CUBE_ARRAY_LEN], permutation move) {
-    sticker_color_t temp1, temp2;
+    sticker_color_t temp1, temp2, temp;
 
     switch (move) {
         case PERM_R:
@@ -20,6 +20,12 @@ void apply_move(sticker_color_t cube[CUBE_ARRAY_LEN], permutation move) {
 
             cube[B0] = temp1;
             cube[B2] = temp2;
+
+            temp = cube[R0];
+            cube[R0] = cube[R2];
+            cube[R2] = cube[R3];
+            cube[R3] = cube[R1];
+            cube[R1] = temp;
             break;
 
         case PERM_R_PRIME:
@@ -37,6 +43,35 @@ void apply_move(sticker_color_t cube[CUBE_ARRAY_LEN], permutation move) {
 
             cube[F1] = temp1;
             cube[F3] = temp2;
+
+            temp = cube[R0];
+            cube[R0] = cube[R1];
+            cube[R1] = cube[R3];
+            cube[R3] = cube[R2];
+            cube[R2] = temp;
+            break;
+
+        case PERM_R2:
+            temp = cube[U1];
+            cube[U1] = cube[D1];
+            cube[D1] = temp;
+            temp = cube[U3];
+            cube[U3] = cube[D3];
+            cube[D3] = temp;
+
+            temp = cube[F1];
+            cube[F1] = cube[B2];
+            cube[B2] = temp;
+            temp = cube[F3];
+            cube[F3] = cube[B0];
+            cube[B0] = temp;
+
+            temp = cube[R0];
+            cube[R0] = cube[R3];
+            cube[R3] = temp;
+            temp = cube[R1];
+            cube[R1] = cube[R2];
+            cube[R2] = temp;
             break;
 
         case PERM_L:
@@ -54,6 +89,12 @@ void apply_move(sticker_color_t cube[CUBE_ARRAY_LEN], permutation move) {
 
             cube[F0] = temp1;
             cube[F2] = temp2;
+
+            temp = cube[L0];
+            cube[L0] = cube[L2];
+            cube[L2] = cube[L3];
+            cube[L3] = cube[L1];
+            cube[L1] = temp;
             break;
 
         case PERM_L_PRIME:
@@ -71,6 +112,35 @@ void apply_move(sticker_color_t cube[CUBE_ARRAY_LEN], permutation move) {
 
             cube[B1] = temp1;
             cube[B3] = temp2;
+
+            temp = cube[L0];
+            cube[L0] = cube[L1];
+            cube[L1] = cube[L3];
+            cube[L3] = cube[L2];
+            cube[L2] = temp;
+            break;
+
+        case PERM_L2:
+            temp = cube[U0];
+            cube[U0] = cube[D0];
+            cube[D0] = temp;
+            temp = cube[U2];
+            cube[U2] = cube[D2];
+            cube[D2] = temp;
+
+            temp = cube[F0];
+            cube[F0] = cube[B3];
+            cube[B3] = temp;
+            temp = cube[F2];
+            cube[F2] = cube[B1];
+            cube[B1] = temp;
+
+            temp = cube[L0];
+            cube[L0] = cube[L3];
+            cube[L3] = temp;
+            temp = cube[L1];
+            cube[L1] = cube[L2];
+            cube[L2] = temp;
             break;
 
         case PERM_U:
@@ -88,6 +158,12 @@ void apply_move(sticker_color_t cube[CUBE_ARRAY_LEN], permutation move) {
 
             cube[L0] = temp1;
             cube[L1] = temp2;
+
+            temp = cube[U0];
+            cube[U0] = cube[U2];
+            cube[U2] = cube[U3];
+            cube[U3] = cube[U1];
+            cube[U1] = temp;
             break;
 
         case PERM_U_PRIME:
@@ -105,6 +181,35 @@ void apply_move(sticker_color_t cube[CUBE_ARRAY_LEN], permutation move) {
 
             cube[R0] = temp1;
             cube[R1] = temp2;
+
+            temp = cube[U0];
+            cube[U0] = cube[U1];
+            cube[U1] = cube[U3];
+            cube[U3] = cube[U2];
+            cube[U2] = temp;
+            break;
+
+        case PERM_U2:
+            temp = cube[F0];
+            cube[F0] = cube[B0];
+            cube[B0] = temp;
+            temp = cube[F1];
+            cube[F1] = cube[B1];
+            cube[B1] = temp;
+
+            temp = cube[R0];
+            cube[R0] = cube[L0];
+            cube[L0] = temp;
+            temp = cube[R1];
+            cube[R1] = cube[L1];
+            cube[L1] = temp;
+
+            temp = cube[U0];
+            cube[U0] = cube[U3];
+            cube[U3] = temp;
+            temp = cube[U1];
+            cube[U1] = cube[U2];
+            cube[U2] = temp;
             break;
 
         case PERM_D:
@@ -122,6 +227,12 @@ void apply_move(sticker_color_t cube[CUBE_ARRAY_LEN], permutation move) {
 
             cube[R2] = temp1;
             cube[R3] = temp2;
+
+            temp = cube[D0];
+            cube[D0] = cube[D2];
+            cube[D2] = cube[D3];
+            cube[D3] = cube[D1];
+            cube[D1] = temp;
             break;
 
         case PERM_D_PRIME:
@@ -139,6 +250,35 @@ void apply_move(sticker_color_t cube[CUBE_ARRAY_LEN], permutation move) {
 
             cube[L2] = temp1;
             cube[L3] = temp2;
+
+            temp = cube[D0];
+            cube[D0] = cube[D1];
+            cube[D1] = cube[D3];
+            cube[D3] = cube[D2];
+            cube[D2] = temp;
+            break;
+
+        case PERM_D2:
+            temp = cube[F2];
+            cube[F2] = cube[B2];
+            cube[B2] = temp;
+            temp = cube[F3];
+            cube[F3] = cube[B3];
+            cube[B3] = temp;
+
+            temp = cube[R2];
+            cube[R2] = cube[L2];
+            cube[L2] = temp;
+            temp = cube[R3];
+            cube[R3] = cube[L3];
+            cube[L3] = temp;
+
+            temp = cube[D0];
+            cube[D0] = cube[D3];
+            cube[D3] = temp;
+            temp = cube[D1];
+            cube[D1] = cube[D2];
+            cube[D2] = temp;
             break;
 
         case PERM_F:
@@ -156,6 +296,12 @@ void apply_move(sticker_color_t cube[CUBE_ARRAY_LEN], permutation move) {
 
             cube[R0] = temp1;
             cube[R2] = temp2;
+
+            temp = cube[F0];
+            cube[F0] = cube[F2];
+            cube[F2] = cube[F3];
+            cube[F3] = cube[F1];
+            cube[F1] = temp;
             break;
 
         case PERM_F_PRIME:
@@ -173,6 +319,35 @@ void apply_move(sticker_color_t cube[CUBE_ARRAY_LEN], permutation move) {
 
             cube[L1] = temp1;
             cube[L3] = temp2;
+
+            temp = cube[F0];
+            cube[F0] = cube[F1];
+            cube[F1] = cube[F3];
+            cube[F3] = cube[F2];
+            cube[F2] = temp;
+            break;
+
+        case PERM_F2:
+            temp = cube[U2];
+            cube[U2] = cube[D1];
+            cube[D1] = temp;
+            temp = cube[U3];
+            cube[U3] = cube[D0];
+            cube[D0] = temp;
+
+            temp = cube[L1];
+            cube[L1] = cube[R0];
+            cube[R0] = temp;
+            temp = cube[L3];
+            cube[L3] = cube[R2];
+            cube[R2] = temp;
+
+            temp = cube[F0];
+            cube[F0] = cube[F3];
+            cube[F3] = temp;
+            temp = cube[F1];
+            cube[F1] = cube[F2];
+            cube[F2] = temp;
             break;
 
         case PERM_B:
@@ -190,6 +365,12 @@ void apply_move(sticker_color_t cube[CUBE_ARRAY_LEN], permutation move) {
 
             cube[L0] = temp1;
             cube[L2] = temp2;
+
+            temp = cube[B0];
+            cube[B0] = cube[B2];
+            cube[B2] = cube[B3];
+            cube[B3] = cube[B1];
+            cube[B1] = temp;
             break;
 
         case PERM_B_PRIME:
@@ -207,6 +388,37 @@ void apply_move(sticker_color_t cube[CUBE_ARRAY_LEN], permutation move) {
 
             cube[R1] = temp1;
             cube[R3] = temp2;
+
+            temp = cube[B0];
+            cube[B0] = cube[B1];
+            cube[B1] = cube[B3];
+            cube[B3] = cube[B2];
+            cube[B2] = temp;
+            break;
+
+        case PERM_B2:
+            temp = cube[U0];
+            cube[U0] = cube[D3];
+            cube[D3] = temp;
+            temp = cube[U1];
+            cube[U1] = cube[D2];
+            cube[D2] = temp;
+
+            temp = cube[R1];
+            cube[R1] = cube[L2];
+            cube[L2] = temp;
+            temp = cube[R3];
+            cube[R3] = cube[L0];
+            cube[L0] = temp;
+
+            temp = cube[B0];
+            cube[B0] = cube[B3];
+            cube[B3] = temp;
+            temp = cube[B1];
+            cube[B1] = cube[B2];
+            cube[B2] = temp;
+            break;
+        default:
             break;
     }
 }
--- src/permutations/permutations.h
+++ src/permutations/permutations.h
@@ -4,18 +4,26 @@
 #define PERMUTATIONS_H
 
 typedef enum {
+    PERM_NONE,
     PERM_R,
     PERM_R_PRIME,
+    PERM_R2,
     PERM_L,
     PERM_L_PRIME,
+    PERM_L2,
     PERM_U,
     PERM_U_PRIME,
+    PERM_U2,
     PERM_D,
     PERM_D_PRIME,
+    PERM_D2,
     PERM_F,
     PERM_F_PRIME,
+    PERM_F2,
     PERM_B,
-    PERM_B_PRIME
+    PERM_B_PRIME,
+    PERM_B2,
+    PERM_COUNT,
 } permutation;
 
 void apply_move(sticker_color_t cube[CUBE_ARRAY_LEN], permutation move);
